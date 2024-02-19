42263f37c68a7d3be340f154a1030d3e00b8572e
Bo <123508131+cbonap1@users.noreply.github.com>
2024-02-19 16:45:20 -0500

    added POST method

--- Woof/Woof/RegisterBusiness.swift
+++ Woof/Woof/RegisterBusiness.swift
@@ -75,7 +75,40 @@ struct RegisterBusiness: View {
         }
     }
 }
-
+private func registerBusiness(businessName: String, businessType: String, location: String, contact: String, description: String, events: String) {
+    let url = URL(string: "http://localhost:8080/register_business")!
+    
+    let body: [String: String] = [
+        "businessName": businessName,
+        "businessType": businessType,
+        "location": location,
+        "contact": contact,
+        "description": description,
+        "events": events
+    ]
+    
+    let jsonData = try? JSONSerialization.data(withJSONObject: body)
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = jsonData
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let httpResponse = response as? HTTPURLResponse {
+            switch httpResponse.statusCode {
+            case 201:
+                print("Business Registered Successfully!")
+            case 500:
+                print("Internal Server Error")
+            default:
+                print("Unexpected error occurred")
+               
+            }
+        }
+    }.resume()
+    
+}
 struct RegisterBusiness_Preview: PreviewProvider {
     static var previews: some View {
         RegisterBusiness()
