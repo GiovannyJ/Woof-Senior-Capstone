aa484a0ebffdb8cbe6a8f0ac39db1a8fbf1f0a0e
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2024-03-14 13:57:40 -0400

    working on 3rd party API

--- 3rdParty_API/woof_API.py
+++ 3rdParty_API/woof_API.py
@@ -7,14 +7,18 @@ class WoofAPI:
         self.url = "localhost:8080/"
 
     def sendBusiness(self, businessObj):
+        url = self.url + "businesses"
         pass
 
     def sendReview(self, reviewObj):
+        url = self.url + f"/user/{reviewObj.userID}/business/{reviewObj.businessID}"
         pass
 
     def sendEvent(self, eventObj):
+        url = self.url + f"events/businesses/{eventObj.businessID}"
         pass
 
     def sendImg(self, imgObj):
+        url = self.url + "uploads"
         pass
 
--- 3rdParty_API/yelp_API.py
+++ 3rdParty_API/yelp_API.py
@@ -3,7 +3,7 @@ This script will pass yelp data to our internal API
 """
 
 import requests
-from dataclasses import dataclass
+import models
 
 class YelpScraper:
     def __init__(self, endpoint, params):
@@ -40,39 +40,3 @@ class YelpScraper:
             pass
         elif self.datatype == "reviews":
             pass
-
-
-@dataclass
-class business():
-    businessName : str
-    businessType: str
-    ownerUserID: int #DEFAULT TO 0 figure out problem later
-    location: str
-    contact: str
-    description: str
-    petSizePref: str #CAN ONLY BE small, medium, large (maybe update to include any -> backend)
-    leashPolicy: bool #i think this is 0 or 1
-    disabledFriendly: bool #i thnk this is 0 or 1
-    dataLocation: str #Default to foreign
-    imgID: int #Default to 0
-
-@dataclass
-class event():
-    eventName: str
-    eventDescription: str
-    eventDate: str
-    location: str
-    contactInfo: str
-    petSizePref: str #CAN ONLY BE small, medium, large (maybe update to include any -> backend)
-    leashPolicy: bool #i think this is 0 or 1
-    disabledFriendly: bool #i thnk this is 0 or 1
-    datalocation: str #Default to foreign
-    imgID: int #Default to 0
-
-@dataclass
-class review():
-    currentuserID: int #default to 0
-    rating: int
-    comment: str
-    datalocation: str #Default to foreign
-    imgID: int #Default to 0
