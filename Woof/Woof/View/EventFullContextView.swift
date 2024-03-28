8bdfaeecf68edfd8db4b9e2f35b17764b0da3f46
Bo <123508131+cbonap1@users.noreply.github.com>
2024-03-28 09:10:03 -0400

    whatcha think!

--- Woof/Woof/View/EventFullContextView.swift
+++ Woof/Woof/View/EventFullContextView.swift
@@ -14,26 +14,47 @@ struct EventFullContextView: View {
     @State private var eventImage: UIImage?
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text(viewModel.event.eventName)
-                .font(.headline)
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Image(systemName:"pawprint.fill")
+                                .font(.title)
+                                .foregroundColor(.orange)
+                            Text(viewModel.event.eventName)
+                                .font(.largeTitle)
+                                .foregroundColor(.orange)
+            };
             Text(viewModel.event.eventDescription)
                 .font(.subheadline)
+                .foregroundColor(.teal)
+            
             Text("Date: \(viewModel.event.eventDate)")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.teal)
+            
             Text("Location: \(viewModel.event.location)")
+                .font(.headline)
+                .foregroundColor(.teal)
+            
             Text("Contact: \(viewModel.event.contactInfo)")
+                .font(.headline)
+                .foregroundColor(.teal)
 
             Text("Attendance Count: \(viewModel.event.attendance_count)")
-                .font(.subheadline)
+                .font(.headline)
+                .foregroundColor(.teal)
 
             Text("Pet Size Preference: \(viewModel.event.petSizePref)")
-                .font(.subheadline)
+                .font(.headline)
+                .foregroundColor(.teal)
 
             Text("Leash Policy: \(viewModel.event.leashPolicy ? "Enforced" : "Not Enforced")")
-                .font(.subheadline)
+                .font(.headline)
+                .foregroundColor(.teal)
 
             Text("Disabled Friendly: \(viewModel.event.disabledFriendly ? "Yes" : "No")")
-                .font(.subheadline)
+                .font(.headline)
+                .foregroundColor(.teal)
 
             if let uiImage = eventImage {
                 Image(uiImage: uiImage)
@@ -46,17 +67,19 @@ struct EventFullContextView: View {
                 viewModel.toggleAttendance()
             }) {
                 Text(viewModel.isAttending ? "Unattend Event" : "Attend Event")
+                    .fontWeight(.light)
                     .foregroundColor(.white)
+                    .font(.callout)
                     .padding()
                     .frame(maxWidth: .infinity)
-                    .background(viewModel.isAttending ? Color.red : Color.teal)
+                    .background(viewModel.isAttending ? Color.red : Color.orange)
                     .cornerRadius(8)
-                    .fontWeight(.bold)
+                    .font(.title)
             }
         }
         .padding()
-        .background(Color.teal.opacity(0.2))
-        .cornerRadius(8)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.teal.opacity(0.2), Color.teal.opacity(0.4)]), startPoint: .top, endPoint: .bottom))
+                .cornerRadius(16)
         .onAppear {
             viewModel.fetchEventImage()
             viewModel.isAttending = sessionManager.eventsAttending?.contains { $0.eventID == viewModel.event.eventID } ?? false
@@ -69,7 +92,6 @@ struct EventFullContextView: View {
         }
     }
 }
-
 
 struct EventFullContextView_Previews: PreviewProvider {
     static var previews: some View {
