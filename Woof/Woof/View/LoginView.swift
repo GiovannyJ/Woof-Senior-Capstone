1cf043b00a4828d0d8b474e16f3bc65527e6fbf8
Bo <123508131+cbonap1@users.noreply.github.com>
2024-04-01 09:08:17 -0400

    login pic

--- Woof/Woof/View/LoginView.swift
+++ Woof/Woof/View/LoginView.swift
@@ -12,67 +12,79 @@ struct LoginView: View {
     @State private var showingAlert = false
 
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 13.0) {
-                TextField("Username", text: $viewModel.username)
-                    .disableAutocorrection(true)
-                    .textContentType(.none)
-                    .padding()
-                    .background(Color.teal.opacity(0.2))
-                    .cornerRadius(8)
-                
-                SecureField("Password", text: $viewModel.password)
-                    .padding()
-                    .background(Color.teal.opacity(0.2))
-                    .cornerRadius(8)
-                
-                Button(action: {
-                    viewModel.authenticateUser()
-                }) {
-                    Text("Login")
-                        .foregroundColor(.white)
+        GeometryReader { geometry in
+            NavigationStack {
+                VStack(spacing: 13.0) {
+                    TextField("Username", text: $viewModel.username)
+                        .disableAutocorrection(true)
+                        .textContentType(.none)
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.teal)
+                        .background(Color.white)
                         .cornerRadius(8)
-                        .fontWeight(Font.Weight.heavy)
+                    
+                    SecureField("Password", text: $viewModel.password)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                    
+                    Button(action: {
+                        viewModel.authenticateUser()
+                    }) {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.teal)
+                            .cornerRadius(8)
+                            .fontWeight(Font.Weight.heavy)
+                    }
+                    .padding(.horizontal)
+                    .padding()
+                    .navigationTitle("Login")
+                    .alert(isPresented: $showingAlert) {
+                        Alert(title: Text("Error"), message: Text(viewModel.errorMessage ?? ""), dismissButton: .default(Text("OK")))
+                    }
+                    
+                    NavigationLink(
+                        destination: RegisterAccountView(),
+                        label: {
+                            Text("Don't have an account? Sign Up")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.red)
+                                .padding(.leading)
+                        })
+                        .padding(.bottom)
+                        .opacity(1.0) // Ensure full opacity
+                    
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .padding(.top, 10)
+                            .opacity(1.0) // Ensure full opacity
+                    }
                 }
                 .padding(.horizontal)
-                .padding()
                 .navigationTitle("Login")
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text("Error"), message: Text(viewModel.errorMessage ?? ""), dismissButton: .default(Text("OK")))
-                }
-                
-                NavigationLink(
-                    destination: RegisterAccountView(),
-                    label: {
-                        Text("Don't have an account? Sign Up")
-                            .font(.subheadline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.red)
-                            .padding(.leading)
-                    })
-                    .padding(.bottom)
-                
-                if let errorMessage = viewModel.errorMessage {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .padding(.top, 10)
-                }
+                .navigationDestination(
+                    isPresented: $viewModel.isLoggedIn) {
+                        HomeView()
+                    }
             }
-            .padding(.horizontal)
-            .padding()
-            .navigationTitle("Login")
-            .navigationDestination(
-                isPresented: $viewModel.isLoggedIn) {
-                    HomeView()
-                }
+            .background(
+                Color.white.opacity(1.0) // Ensure full opacity
+            )
+            .overlay(
+                Image("Image 1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(0.3)
+            )
         }
     }
 }
-
-
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
